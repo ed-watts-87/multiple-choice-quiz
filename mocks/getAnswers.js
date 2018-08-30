a15afa38501c92750a3659566eafec60555a64d4
Ed Watts <ed.watts@gmail.com>
2018-08-30 15:19:09 +0100

    break out sort from main function, simplify return obj generation

--- mocks/getAnswers.js
+++ mocks/getAnswers.js
@@ -1,31 +1,29 @@
 import answers from "./answers";
 
+const sortAnswers = (a, b) => {
+  const indexA = a.question;
+  const indexB = b.question;
+  let comparison = 0;
+  if (indexA > indexB) {
+    comparison = 1;
+  } else if (indexA < indexB) {
+    comparison = -1;
+  }
+  return comparison;
+};
+
 const getAnswers = givenAnswers =>
   new Promise((resolve, reject) => {
-    const sortAnswers = (a, b) => {
-      const indexA = a.question;
-      const indexB = b.question;
-      let comparison = 0;
-      if (indexA > indexB) {
-        comparison = 1;
-      } else if (indexA < indexB) {
-        comparison = -1;
-      }
-      return comparison;
-    };
     const checkResults = givenAnswers.map((givenAnswer, idx) => {
       const indexOfAnswer = givenAnswer.questionIndex;
-      if (givenAnswer.answer === answers[indexOfAnswer].answer) {
-        return {
-          question: indexOfAnswer,
-          result: "correct"
-        };
-      } else {
-        return {
-          question: indexOfAnswer,
-          result: "incorrect"
-        };
-      }
+      return {
+        question: indexOfAnswer,
+        questionText: answers[indexOfAnswer].questionText,
+        result:
+          givenAnswer.answer === answers[indexOfAnswer].answer
+            ? "correct"
+            : "incorrect"
+      };
     });
 
     const results = checkResults.sort(sortAnswers);
