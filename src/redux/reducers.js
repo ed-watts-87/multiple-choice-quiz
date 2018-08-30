f9936810cf58ea9a02443710b5dd9d0549cf9b42
Ed Watts <ed.watts@gmail.com>
2018-08-30 12:52:26 +0100

    add answer reducers and tests

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,7 +1,7 @@
 import { RETRIEVE_QUESTIONS, ANSWER_QUESTION } from "./actions";
 import { combineReducers } from "redux";
 
-function questionsReducer(state = {}, action) {
+export const questionsReducer = (state = [], action) => {
   switch (action.type) {
     case RETRIEVE_QUESTIONS: {
       return action.payload;
@@ -9,10 +9,34 @@ function questionsReducer(state = {}, action) {
     default:
       return state;
   }
-}
+};
+
+export const answerReducer = (state = [], action) => {
+  switch (action.type) {
+    case ANSWER_QUESTION:
+      console.log(action);
+      const findAnswerIndex = el =>
+        el.questionIndex == action.answer.questionIndex;
+      const answerExists = state.findIndex(findAnswerIndex) > -1;
+      let answersArray = state.slice();
+      if (answerExists) {
+        answersArray = answersArray.map(answer => {
+          return answer.questionIndex == action.answer.questionIndex
+            ? action.answer
+            : answer;
+        });
+      } else {
+        answersArray.push(action.answer);
+      }
+      return answersArray;
+    default:
+      return state;
+  }
+};
 
 const reducer = combineReducers({
-  questions: questionsReducer
+  questions: questionsReducer,
+  answers: answerReducer
 });
 
 export default reducer;
---
+++ src/redux/reducers.test.js
@@ -0,0 +1,50 @@
+import { questionsReducer, answerReducer } from "./reducers";
+import questions from "../../mocks/questions.json";
+
+const finalQuestionState = questions;
+
+const answerAddState = [
+  {
+    answer: "Soup",
+    questionIndex: 0
+  }
+];
+
+const answerUpdateState = [
+  {
+    answer: "Potato",
+    questionIndex: 0
+  }
+];
+
+test("expect final question state to be list of questions", () => {
+  const action = {
+    type: "RETRIEVE_QUESTIONS",
+    payload: questions
+  };
+  expect(questionsReducer([], action)).toEqual(finalQuestionState);
+});
+
+test("expect final answer state to have 1 answer", () => {
+  const answer = {
+    questionIndex: 0,
+    answer: "Soup"
+  };
+  const action = {
+    type: "ANSWER_QUESTION",
+    answer: answer
+  };
+  expect(answerReducer([], action)).toEqual(answerAddState);
+});
+
+test("expect final answer state to have updated answer", () => {
+  const answer = {
+    questionIndex: 0,
+    answer: "Potato"
+  };
+  const action = {
+    type: "ANSWER_QUESTION",
+    answer: answer
+  };
+  expect(answerReducer(answerAddState, action)).toEqual(answerUpdateState);
+});
